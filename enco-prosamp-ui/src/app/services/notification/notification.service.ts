44ff9e5422f8befa8bfd0b30d5b0d4f25d878577
joooki82 <61735967+joooki82@users.noreply.github.com>
2025-03-21 19:38:27 +0100

    toaster configuration, notification service

--- enco-prosamp-ui/src/app/services/notification/notification.service.ts
+++ enco-prosamp-ui/src/app/services/notification/notification.service.ts
@@ -1,34 +1,36 @@
-import { Injectable } from '@angular/core';
-import { Subject } from 'rxjs';
-import {Toast} from "../../shared/components/toasters/app-toast-sample/toast.model";
+import {Injectable, viewChild} from '@angular/core';
+import {AppToastSampleComponent} from "../../shared/components/toasters/app-toast-sample/app-toast-sample.component";
+import {ToasterComponent, ToasterPlacement} from "@coreui/angular";
 
-
-@Injectable({ providedIn: 'root' })
-
+@Injectable({
+  providedIn: 'root'
+})
 export class NotificationService {
-  private toasts: Toast[] = [];
-  private toastSubject = new Subject<Toast[]>();
-  toastUpdates$ = this.toastSubject.asObservable();
-  private idCounter = 0;
 
-  showToast(type: Toast['type'], message: string, duration = 5000) {
-    const toast: Toast = {
-      id: ++this.idCounter,
-      type,
-      message,
-      duration
-    };
-    this.toasts.push(toast);
-    this.toastSubject.next(this.toasts);
-    console.log('Toast:', toast);
-    setTimeout(() => this.removeToast(toast.id), duration);
+  private toasterComponent: ToasterComponent | null = null;
+
+  setToasterComponent(toaster: ToasterComponent) {
+    this.toasterComponent = toaster;
   }
 
-  removeToast(id: number) {
-    console.log(`Removing toast with ID ${id}`);
-    this.toasts = this.toasts.filter(t => t.id !== id);
-    console.log('Remaining toasts after removal:', this.toasts);
-    this.toastSubject.next([...this.toasts]);  // 🛠 Emit new array reference
+  showToast(options: {
+    title?: string;
+    color?: string;
+    delay?: number;
+    autohide?: boolean;
+  }) {
+    if (!this.toasterComponent) {
+      console.warn('Toaster component not set.');
+      return;
+    }
+
+    this.toasterComponent.addToast(AppToastSampleComponent, {
+      title: options.title ?? 'Info',
+      color: options.color ?? 'info',
+      autohide: options.autohide ?? true,
+      delay: options.delay ?? 5000,
+      placement: ToasterPlacement.TopCenter
+    });
   }
 
 }
